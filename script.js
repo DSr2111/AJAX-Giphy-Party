d74cb144a50b54958f5a31bbfb7213ec61e116bf
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-14 17:13:17 -0500

    final edits to JS, cleaned up functions

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ const ul = document.getElementById("gifList");
 async function getGif(searchTerm) {
   //ajax function to search GIPHY API
   const url = `https://api.giphy.com/v1/gifs/random?api_key=07ynKbEJm48vdxl0BPiyB1hFyXjstKn8&tag=${searchTerm}&rating=pg`;
-  const res = await axios.get(url);
-  console.log(res);
-  console.log(res.data.data.url);
-  let newLI = document.createElement("li");
-  newLI.innerHTML = `<img class="gifs" src=${res.data.data.images.original.url} alt="${searchTerm} GIF">`;
-  ul.append(newLI);
-  input.value = "";
+  let gifUrl;
+
+  try {
+    const res = await axios.get(url);
+    gifUrl = res.data.data.images.original.url;
+  } catch (error) {
+    console.error("Error fetching GIF", error);
+  }
+  if (gifUrl) {
+    addGif(gifUrl, searchTerm);
+  }
 }
 
-function addGif() {} //appending gifs to UL
+function addGif(gifUrl, searchTerm) {
+  let newLI = document.createElement("li");
+  newLI.innerHTML = `<img class="gifs" src=${gifUrl} alt="${searchTerm} GIF">`;
+  ul.append(newLI);
+  input.value = "";
+} //appending gifs to UL
 
 function removeGifs() {
   //removing all gifs in UL
