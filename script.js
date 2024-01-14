1078ceb2450a279ffe5a9d3b591fe97fe95bfb10
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-14 13:42:28 -0500

    JS functions

---
+++ script.js
@@ -0,0 +1,8 @@
+const remove = document.getElementById("remove-btn");
+const submit = document.getElementById("submitSearch");
+
+async function getGif() {} //ajax function to search GIPHY API
+
+function addGif() {} //appending gifs to UL
+
+function removeGifs() {} //removing all gifs in UL
