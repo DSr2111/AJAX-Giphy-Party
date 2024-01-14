d5b3092e173082c7c5bbdc7d94d903714cc25719
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-14 15:50:17 -0500

    more JS edits, functions, evt listeners

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const remove = document.getElementById("remove-btn");
-const submit = document.getElementById("submitSearch");
+async function getGif(searchTerm) {
+  //ajax function to search GIPHY API
 
-async function getGif() {} //ajax function to search GIPHY API
+  const res = await axios.get("api.giphy.com/v1/gifs/search", {
+    params: { searchTerm },
+  });
+}
 
 function addGif() {} //appending gifs to UL
 
 function removeGifs() {} //removing all gifs in UL
+
+const remove = document.getElementById("remove-btn");
+const form = document.getElementById("searchForm");
+const input = document.getElementById("gifSearch");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  getGif(input.value);
+});
+
+remove.addEventListener("click", function (e) {
+  e.preventDefault();
+  removeGifs();
+});
